323867a015861ddc9a33b3fc0e9e03948488599d
Mohamed Taha Bayoumi <mohamed.bayoumi@telecom-paris.fr>
2022-09-02 21:28:30 +0200

    cards and grid layout

---
+++ teams/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import * as React from 'react' ;
+import Header from './Header';
+import Employees from './Employees';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <Employees/>
+      <Footer/>
+    </div>
+  );
+}
+
+export default App;
